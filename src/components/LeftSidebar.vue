007a00df28d21c62c8f65d1b73ec27d12aec0074
Joe Santoru <joseph.santoru@thomsonreuters.com>
2017-10-27 15:30:26 -0400

    added regular bootstrap and sidebar highlighting on click

--- src/components/LeftSidebar.vue
+++ src/components/LeftSidebar.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="left-sidebar">
-    <h1>{{ msg }}</h1>
+    <h3>Sidebar header</h3>
+    <ul class="nav nav-pills flex-column">
+      <li v-for="menuItem in menuItems" nav-item>
+        <a class="nav-link" href="#" v-on:click="setActive(menuItem)" v-bind:class="{'bold': isActive(menuItem)}">{{menuItem.name}}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,8 +14,29 @@
     name: 'LeftSidebar',
     data () {
       return {
-        msg: 'This is the left sidebar'
+        msg: 'This is the left sidebar',
+        selectedItemId: 1,
+        menuItems: [
+          {
+              id: 1,
+              name: "One"
+          },
+          {
+              id: 2,
+              name: "Two"
+          }
+        ]
       }
+    },
+    methods: {
+        setActive: function (menuItem) {
+            console.log(menuItem);
+            this.selectedItemId = menuItem.id;
+        },
+        isActive: function (menuItem) {
+          console.log("isActive?", menuItem);
+          return this.selectedItemId == menuItem.id;
+        }
     }
   }
 </script>
@@ -23,5 +49,9 @@
     width: 20%;
     height: 100%;
     overflow:auto;
+  }
+
+  .bold {
+    font-weight:bold;
   }
 </style>
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,45 +1,40 @@
 <template>
   <div id="top-nav">
-    <b-navbar toggleable="md" type="dark" variant="info" fixed="top">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
-
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-      <b-collapse is-nav id="nav_collapse">
-
-        <b-nav is-nav-bar>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-nav>
-
-        <!-- Right aligned nav items -->
-        <b-nav is-nav-bar class="ml-auto">
-
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using button-content slot -->
-            <template slot="button-content">
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Signout</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-nav>
-
-      </b-collapse>
-    </b-navbar>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
   </div>
 </template>
 
